1e41e2c05b2a8c283dadfbf2ee0ce09f7806900b
koray öngün <koraygg28@gmail.com>
2023-03-11 18:38:11 +0300

    Proje dosyası ekle.

---
+++ Okuma.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberReading
+{
+    public class Okuma
+    {
+
+
+        string[] birler = { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+        string[] onlar = { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+        string yuz = "yüz";
+        string[] bm = { "", "bin", "milyon", "milyar", "trilyon", "katrilyon", "kentilyon" };
+        public void Bol(ref string s)
+        {
+            int j = 0;
+            for (int i = s.Length; i >= 0; i--)
+            {
+                bool b = (j != 0) && (j % 3 == 0) && (i != 0);
+                if (b) s = s.Insert(i, ",");
+                j++;
+            }
+        }
+        public string Oku(string sNumber)
+        {
+            //sNumber = 1,123,076
+
+            string[] numbers = sNumber.Split(',');
+            // numbers = {"1","123","076"}
+
+
+            string result = string.Empty;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int _number = Convert.ToInt32(numbers[i]);
+                int t = numbers.Length - (i + 1);
+
+                int x, y, z;
+                // x = yüzler,y=onlar,b=birler
+
+                x = _number / 100;
+                y = (_number % 100) / 10;
+                z = _number % 10;
+
+                if (x != 0)
+                {
+                    result += x == 1 ? $"{yuz} " : $"{birler[x]}{yuz} ";
+                }
+                result += $"{onlar[y]} {birler[z]} ";
+                result += $"{bm[t]} ";
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using NumberReading;
+
+Okuma okuma = new Okuma();
+string result = string.Empty;
+try
+{
+    ulong number = Convert.ToUInt64(Console.ReadLine());
+    if (number == 0)
+    {
+        Console.WriteLine("sıfır");
+    }
+    else
+    {
+        string sNumber = number.ToString();
+        //sNumber = "1123076"
+
+        okuma.Bol(ref sNumber);
+        // sNumber = "1,123,076
+
+        result = okuma.Oku(sNumber);
+
+        Console.WriteLine(result);
+        Console.WriteLine(sNumber);
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("0-18446744073709551615 aralığında bir değer girin.");
+
+}
+
